1b85988b024a68e7ad0f197502d6912bea244c7d
ysaimanideep523-alt <ysaimanideep523@gmail.com>
2026-03-16 21:53:07 +0530

    added level1

---
+++ Birthyear.java
@@ -0,0 +1,13 @@
+/* 
+@author Manideep
+ */
+public class Birthyear {
+    public static void main(String[] args) {
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
