92ad7f368dbf9d5390886c8b03c603f1f5b3f6fe
ysaimanideep523-alt <ysaimanideep523@gmail.com>
2026-03-16 22:01:27 +0530

    added level1

---
+++ Convertdistance.java
@@ -0,0 +1,11 @@
+/* 
+@author Manideep
+ */
+ public class Convertdistance {
+    public static void main(String[] args) {
+        double km = 10.8;
+        double miles = km * 1.6;
+
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}
